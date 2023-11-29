096ee69a2c44404d5375510942ac656804a76313
master2it <mahdavi2it@gmail.com>
2023-11-29 18:21:50 +0300

    init update customer form

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="p-5">
     <div v-if="type === 'list'">
-      <button class="bg-blue-500 text-white p-2 rounded" @click="type = 'form'">Create a new Customer</button>
-      <Customers/>
+      <button class="bg-blue-500 text-white p-2 rounded" @click="()=> {
+        type = 'form';
+        formType = 'create'
+      }">Create a new Customer
+      </button>
+      <Customers :customers="customersData" @handleEdit="handleEdit" @handleRemove="handleRemove"/>
     </div>
-    <Form v-else-if="type === 'form'" type="Create" @handleSubmit="handleSubmit"/>
+    <Form v-else-if="type === 'form'" :email="emailForUpdate" :type="formType" @handleCancel="type = 'list'"
+          @handleSubmit="handleSubmit"/>
   </div>
 </template>
 
@@ -12,8 +17,26 @@
 import Customers from "@/components/Customers.vue";
 import {ref} from "vue";
 import Form from "@/components/Form.vue";
+import {getAll} from "@/services/storage";
 
-const type = ref('form')
+const type = ref('list') // list || form
+const formType = ref('create') // create || update
+const customersData = ref(getAll())
+const emailForUpdate = ref(null)
+
+const handleSubmit = () => {
+  type.value = 'list'
+  customersData.value = getAll()
+}
+const handleRemove = () => {
+  customersData.value = getAll()
+}
+const handleEdit = email => {
+  formType.value = 'update'
+  type.value = 'form'
+  emailForUpdate.value = email
+}
+
 </script>
 
 <style>
