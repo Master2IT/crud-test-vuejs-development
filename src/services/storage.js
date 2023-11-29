8e99e5d9a31d7fd8f08176f7955bfa359701a1e4
master2it <mahdavi2it@gmail.com>
2023-11-29 17:05:44 +0300

    update storage and define a local key

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,24 +1,26 @@
-export const add = ({data, name})=>{
-    const storage = window.localStorage.getItem(name) || [];
+const key = "customers"
+
+export const add = (data) => {
+    const storage = JSON.parse(window.localStorage.getItem(key)) || []
     storage.push(data);
-    window.localStorage.setItem(name, JSON.stringify(storage))
+    window.localStorage.setItem(key, JSON.stringify(storage))
 }
-export const update = ({data, name})=>{
-    const storage = window.localStorage.getItem(name);
-    const findDataIndex = storage.findIndex(ex=> ex.email === data.email);
-    if(findDataIndex){
+export const update = (data) => {
+    const storage = JSON.parse(window.localStorage.getItem(key))
+    const findDataIndex = storage.findIndex(ex => ex.email === data.email);
+    if (findDataIndex) {
         storage[findDataIndex] = data;
-        window.localStorage.setItem(name, JSON.stringify(storage))
+        window.localStorage.setItem(key, JSON.stringify(storage))
     }
 }
-export const remove = ({email, name})=> {
-    const storage = window.localStorage.getItem(name);
+export const remove = (email) => {
+    const storage = JSON.parse(window.localStorage.getItem(key))
     const findDataIndex = storage.findIndex(ex => ex.email === email);
     storage.splice(findDataIndex, 1)
-    window.localStorage.setItem(name, JSON.stringify(storage))
+    window.localStorage.setItem(key, JSON.stringify(storage))
 }
-export const getOne = ({email, name})=> {
-    const storage = window.localStorage.getItem(name);
+export const getOne = (email) => {
+    const storage = JSON.parse(window.localStorage.getItem(key)) || []
 
-    return storage.find(item=> item.email === email) || null
+    return storage.find(item => item.email === email) || null
 }
