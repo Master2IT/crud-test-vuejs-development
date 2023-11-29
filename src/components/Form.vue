4a7ba8365cab5c2b7140064d164f5f691f4773c3
master2it <mahdavi2it@gmail.com>
2023-11-29 18:21:19 +0300

    init update customer form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,6 +1,6 @@
 <template>
   <form class="form flex flex-col gap-5" @submit="handleSubmitForm">
-    <h1>{{ type }} Customer</h1>
+    <h1><span class="capitalize">{{ type }}</span> Customer</h1>
     <input v-model="form.firstName" class="p-3 w-full border rounded" placeholder="First Name"
            type="text"/>
     <input v-model="form.lastName" class="p-3 w-full border rounded" placeholder="Last Name" type="text"/>
@@ -12,15 +12,18 @@
     <input v-model="form.bankAccountNumber" class="p-3 w-full border rounded"
            placeholder="Bank Account Number"
            type="text"/>
-    <button class="p-2 rounded bg-blue-500 text-white">Submit</button>
+    <div class="flex gap-2 justify-end">
+      <button class="p-2 rounded bg-gray-500 text-white" type="button" @click="emit('handleCancel')">Cancel</button>
+      <button class="p-2 px-10 rounded bg-blue-500 text-white">Submit</button>
+    </div>
   </form>
 </template>
 <script setup>
-import {ref, defineEmits, defineProps} from 'vue'
-import {getOne} from "@/services/storage";
+import {ref, defineEmits, defineProps, onMounted} from 'vue'
+import {addToStorage, getOne, updateStorage} from "@/services/storage";
 
-const {type} = defineProps(['type']);
-const emit = defineEmits(['handleSubmit'])
+const {type, email} = defineProps(['type', 'email']);
+const emit = defineEmits(['handleSubmit', 'handleCancel'])
 
 const form = ref({
   firstName: '',
@@ -29,6 +32,21 @@ const form = ref({
   phoneNumber: '',
   email: '',
   bankAccountNumber: ''
+})
+
+onMounted(() => {
+  if (type === 'update') {
+    email && (form.value = getOne(email))
+  } else {
+    form.value = {
+      firstName: '',
+      lastName: '',
+      dateOfBirth: '',
+      phoneNumber: '',
+      email: '',
+      bankAccountNumber: ''
+    }
+  }
 })
 
 const validateForm = () => {
@@ -57,7 +75,7 @@ const validateForm = () => {
   const customer = getOne(form.value.email);
 
   // Validate email
-  if (customer) {
+  if (customer && customer.email !== form.value.email) {
     alert("Email is duplicated!")
     isValid = false;
   }
@@ -81,7 +99,16 @@ const handleSubmitForm = e => {
   e.preventDefault()
 
   if (validateForm()) {
-
+    if (type === 'create') {
+      addToStorage(form.value);
+      alert('Customer created successfully.')
+      emit('handleSubmit')
+    }
+    if (type === 'update') {
+      updateStorage(form);
+      alert('Customer updated successfully.')
+      emit('handleSubmit')
+    }
   }
 }
 
